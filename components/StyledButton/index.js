587be0b29681b7626b1df8de323cbe19c5b9ec8c
Ka Wing Tsang <kawingt2@gmail.com>
2021-11-25 11:50:47 +0800

    set up props for StyledButton component

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { View, Text, Pressable } from 'react-native';
 import styles from './styles';
 
-const StyledButton = () => {
+const StyledButton = (props) => {
+	const { type, content, onPress } = props;
+
+	const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
+	const textColor = type === 'primary' ? '#FFFFFF' : '#171A20';
+
 	return (
 		<View style={styles.container}>
 			<Pressable
-				style={styles.button}
-				onPress={() => {
-					console.warn('Hey There');
-				}}
+				style={[styles.button, { backgroundColor: backgroundColor }]}
+				onPress={() => onPress()}
 			>
-				<Text style={styles.text}>Custom Order</Text>
+				<Text style={[styles.text, { color: textColor }]}>
+					{content}
+				</Text>
 			</Pressable>
 		</View>
 	);
