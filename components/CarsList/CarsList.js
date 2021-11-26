8db559cf957ccbf64d5011475aacef4796b559fc
Ka Wing Tsang <kawingt2@gmail.com>
2021-11-26 11:33:13 +0800

    render a list of cars in CarsList

--- components/CarsList/CarsList.js
+++ components/CarsList/CarsList.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import styles from './styles';
+import CarItem from '../CarItem/CarItem';
 
 // Car data
 import cars from './cars';
 
 function CarsList() {
 	return (
-		<View>
-			<Text>CarsList</Text>
+		<View style={styles.container}>
+			<FlatList
+				data={cars}
+				renderItem={({ item }) => <CarItem car={item} />}
+			/>
 		</View>
 	);
 }
